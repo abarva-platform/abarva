e9f03f36893c1b098844902b2e79050d01e9589b
anandsundaram-hash <anand.sundaram@thesundaram.com>
2026-06-01 10:10:06 -0500

    feat(programs): add quarterly board pack renderer (#2699)
    Co-authored-by: Anand Sundaram <anandsundaram-hash@users.noreply.github.com>

---
+++ src/lib/programs/expert-kernel/exports/board-pack/index.ts
@@ -0,0 +1,20 @@
+export {
+  buildQuarterlyBoardPack,
+  type QuarterlyBlockedDecision,
+  type QuarterlyBoardPack,
+  type QuarterlyBoardPackInput,
+  type QuarterlyBoardPackSection,
+  type QuarterlyBoardQuestion,
+  type QuarterlyMoveStatus,
+  type QuarterlyPattern,
+  type QuarterlyRiskLine,
+  type QuarterlySequenceStep,
+} from './quarterly-board-pack-model';
+export {
+  renderBoardPackHtml,
+  renderQuarterlyBoardPackHtml,
+} from './html-renderer';
+export {
+  buildBoardPackPdfElement,
+  buildQuarterlyBoardPackPdfElement,
+} from './pdf-renderer';
